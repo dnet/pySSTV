10c85423d49c9aad12baf1d4bd9ef8eedc5cbcbf
András Veres-Szentkirályi <vsza@vsza.hu>
2013-07-02 15:18:12 +0200

    removed unused import math.floor

--- pysstv/sstv.py
+++ pysstv/sstv.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 from __future__ import division, with_statement
-from math import sin, pi, floor
+from math import sin, pi
 from random import random
 from contextlib import closing
 from itertools import imap, izip, cycle, chain
