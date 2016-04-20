23a2e2bd00b53566c067e76c4fa220be756e4e95
András Veres-Szentkirályi <vsza@vsza.hu>
2016-04-20 19:36:24 +0200

    codegen: check for non-RGB images

--- pysstv/examples/codeman.c
+++ pysstv/examples/codeman.c
@@ -7,6 +7,10 @@
 int main(int argc, char **argv) {
 	int x, y, n;
 	unsigned char *img = stbi_load(argv[1], &x, &y, &n, 0);
+	if (n != 3) {
+		fprintf(stderr, "Only RGB images are supported\n");
+		return 1;
+	}
 
 	float freqs[FREQ_COUNT], msecs[FREQ_COUNT];
 
