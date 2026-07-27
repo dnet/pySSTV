2d5c03538588d48d773cd67d2d75f42913a6349b
András Veres-Szentkirályi <vsza@vsza.hu>
2026-07-27 09:47:14 +0200

    bumped version to v0.5.9

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='PySSTV',
-    version='0.5.8',
+    version='0.5.9',
     description='Python classes for generating Slow-scan Television transmissions',
     author=u'András Veres-Szentkirályi',
     author_email='vsza@vsza.hu',
